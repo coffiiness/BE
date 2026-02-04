79cbb8414a84ca77a0546e9c313ba095ec5770c5
haengguk <guddnrb14@naver.com>
2026-02-04 16:17:07 +0900

    feat: [BE] 채용 엔티티 작성

---
+++ storage/db-core/src/main/java/com/team/voteland/storage/db/core/recruitment/RecruitmentEntity.java
@@ -0,0 +1,36 @@
+package com.team.voteland.storage.db.core.recruitment;
+
+import com.team.voteland.storage.db.core.BaseEntity;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "recruitments")
+public class RecruitmentEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long workspaceId; // 워크스페이스 ID
+
+    @Column(nullable = false)
+    private Long creatorId; // 채용 작성자 ID
+
+    @Column(nullable = false)
+    private String title; // 채용 제목
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RecruitmentStatus status; // 공고 상태(대기, 진행중, 닫힘)
+
+    @Column(nullable = false)
+    private int targetCount; // 채용 인원
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+}
