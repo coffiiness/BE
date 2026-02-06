bb99bc1ebb4e571bb7bc1c379b316265236ef798
ucb1122 <ucb1122@naver.com>
2026-02-06 09:11:49 +0900

    feat[BE]: 지원서, 지원자, 지원서  템플릿 엔티티 작성

---
+++ storage/db-core/src/main/java/com/coffiness/calfit/storage/db/core/applicant/ApplicantEntity.java
@@ -0,0 +1,28 @@
+package com.coffiness.calfit.storage.db.core.applicant;
+
+import com.coffiness.calfit.core.enums.UserRole;
+import com.coffiness.calfit.storage.db.core.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "applicants") // 지원자
+@NoArgsConstructor
+@Getter
+public class ApplicantEntity extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+}
---
+++ storage/db-core/src/main/java/com/coffiness/calfit/storage/db/core/template/ApplicationTemplateEntity.java
@@ -0,0 +1,44 @@
+package com.coffiness.calfit.storage.db.core.template;
+
+import com.coffiness.calfit.core.enums.Gender;
+import com.coffiness.calfit.storage.db.core.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "templates") // 템플릿
+@NoArgsConstructor
+@Getter
+public class ApplicationTemplateEntity extends BaseEntity {
+
+    // 지원자 이름
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    // 지원자 성별
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    // 지원자 생년월일
+    @Column(nullable = false)
+    private LocalDateTime birthDate;
+
+    // 지원자 전화번호
+    @Column(nullable = false, length = 20)
+    private String phone;
+
+    // 지원자 이메일
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    // 지원자 상세내용
+    @Column(name = "schema", columnDefinition = "JSON", nullable = false)
+    private String schema;
+
+    @Column(name = "is_default")
+    private Boolean isDefault = false;
+}
