5fbed35b8a44c52695dda84932d49a48670d0083
haengguk <guddnrb14@naver.com>
2026-02-23 17:27:53 +0900

    feat: [BE] 외부 캘린더, 내부 내 일정 캘린더 DB 엔티티 작성 (#14)

---
+++ core/core-enum/src/main/java/com/coffiness/calfit/core/enums/ScheduleType.java
@@ -0,0 +1,15 @@
+package com.coffiness.calfit.core.enums;
+
+public enum ScheduleType {
+
+    MEETING("회의"),
+    VACATION("휴가"),
+    BUSINESS("외근/출장"),
+    OTHERS("기타");
+
+    private String description;
+
+    private ScheduleType(final String description) {
+        this.description = description;
+    }
+}
