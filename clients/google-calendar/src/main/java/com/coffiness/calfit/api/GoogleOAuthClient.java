51e149a4ad7eeba9dd947be399596535b4068594
haengguk <guddnrb14@naver.com>
2026-02-25 15:05:46 +0900

    feat: [BE] 구글 캘린더 엔티티 토큰 메소드 추가 및 레포지토리 추가 (#23)

--- clients/google-calendar/src/main/java/com/coffiness/calfit/api/GoogleOAuthClient.java
+++ clients/google-calendar/src/main/java/com/coffiness/calfit/api/GoogleOAuthClient.java
@@ -11,29 +11,25 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class GoogleOAuthClient implements GoogleOAuthPort {
 
-    private final GoogleOAuthApi googleOAuthApi;
+  private final GoogleOAuthApi googleOAuthApi;
 
-    @Value("${spring.security.oauth2.client.registration.google.client-id}")
-    private String clientId;
+  @Value("${spring.security.oauth2.client.registration.google.client-id}")
+  private String clientId;
 
-    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
-    private String clientSecret;
+  @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+  private String clientSecret;
 
-    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
-    private String redirectUri;
+  @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+  private String redirectUri;
 
-    public GoogleTokenModel exchangeToken(String authCode) {
+  public GoogleTokenModel exchangeToken(String authCode) {
 
-        // 구글 스펙상 고정값
-        final String GRANT_TYPE = "authorization_code";
+    // 구글 스펙상 고정값
+    final String GRANT_TYPE = "authorization_code";
 
-        GoogleOAuthResponseDto rawDto = googleOAuthApi.exchange(authCode, clientId, clientSecret, redirectUri, GRANT_TYPE);
+    GoogleOAuthResponseDto rawDto =
+        googleOAuthApi.exchange(authCode, clientId, clientSecret, redirectUri, GRANT_TYPE);
 
-        return new GoogleTokenModel(
-                rawDto.accessToken(),
-                rawDto.refreshToken(),
-                rawDto.expiresIn()
-        );
-
-    }
+    return new GoogleTokenModel(rawDto.accessToken(), rawDto.refreshToken(), rawDto.expiresIn());
+  }
 }
--- core/domain-calendar/src/main/java/com/coffiness/calfit/port/GoogleOAuthPort.java
+++ core/domain-calendar/src/main/java/com/coffiness/calfit/port/GoogleOAuthPort.java
@@ -3,5 +3,5 @@ package com.coffiness.calfit.port;
 import com.coffiness.calfit.model.GoogleTokenModel;
 
 public interface GoogleOAuthPort {
-    GoogleTokenModel exchangeToken(String authCode);
+  GoogleTokenModel exchangeToken(String authCode);
 }
