9c338864996556afb4595c382489484c73816f0e
haengguk <guddnrb14@naver.com>
2026-02-25 11:24:21 +0900

    refactor: [BE] 코드 포맷팅 수정 (#4)

--- core/domain-calendar/src/main/java/com/coffiness/calfit/port/GoogleCalendarPort.java
+++ core/domain-calendar/src/main/java/com/coffiness/calfit/port/GoogleCalendarPort.java
@@ -2,19 +2,17 @@ package com.coffiness.calfit.port;
 
 import com.coffiness.calfit.model.GoogleCalendarClientResult;
 import com.coffiness.calfit.model.GoogleCalendarSyncResult;
-
 import java.time.ZonedDateTime;
 
 public interface GoogleCalendarPort {
 
-    GoogleCalendarClientResult createEvent(
-            String accessToken,
-            String summary,
-            String description,
-            ZonedDateTime start,
-            ZonedDateTime end,
-            boolean allDay
-    );
+  GoogleCalendarClientResult createEvent(
+      String accessToken,
+      String summary,
+      String description,
+      ZonedDateTime start,
+      ZonedDateTime end,
+      boolean allDay);
 
-    GoogleCalendarSyncResult syncEvent(String accessToken, String syncToken);
+  GoogleCalendarSyncResult syncEvent(String accessToken, String syncToken);
 }
