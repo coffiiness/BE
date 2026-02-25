9588b7ddffc853ba0979b9683ea76ae563741047
haengguk <guddnrb14@naver.com>
2026-02-25 15:13:46 +0900

    feat: [BE] 레포지토리 tenantId 추가 (#23)

--- storage/db-core/src/main/java/com/coffiness/calfit/storage/db/core/calendar/ExternalCalendarRepository.java
+++ storage/db-core/src/main/java/com/coffiness/calfit/storage/db/core/calendar/ExternalCalendarRepository.java
@@ -1,14 +1,15 @@
 package com.coffiness.calfit.storage.db.core.calendar;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExternalCalendarRepository extends JpaRepository<ExternalCalendarEntity, Long> {
 
   // 특정 유저의 모든 외부 캘린더 연동 정보 조회
-  List<ExternalCalendarEntity> findAllByUserId(Long userId);
+  List<ExternalCalendarEntity> findAllByTenantIdAndUserId(String tenantId, Long userId);
 
   // 특정 유저의 특정 외부 캘린더 단건 조회
-  Optional<ExternalCalendarEntity> findByUserIdAndCalendarId(Long userId, String calendarId);
+  Optional<ExternalCalendarEntity> findByTenantIdAndUserIdAndCalendarId(String tenantId, Long userId, String calendarId);
 }
