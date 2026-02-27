b946fbe0aa682f9e659fcc786da6c9a9f8784f9e
ucb1122 <ucb1122@naver.com>
2026-02-27 09:36:43 +0900

    refactor: [BE] 회의실 관련 리펙토링

--- storage/db-core/src/main/java/com/coffiness/calfit/storage/db/core/meetingRoom/MeetingRoomRepository.java
+++ storage/db-core/src/main/java/com/coffiness/calfit/storage/db/core/meetingRoom/MeetingRoomRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MeetingRoomRepository extends JpaRepository<MeetingRoomEntity, Long> {
 
-
   boolean existsByName(String name);
 
   Optional<MeetingRoomEntity> findByName(String name);
