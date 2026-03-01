2f1d8314dec28c1184f4ea266dc9fe719e96234c
haengguk <guddnrb14@naver.com>
2026-03-01 20:50:23 +0900

    feat: [BE] 내 일정 수정 API 작성 (#50)

--- storage/db-core/src/main/java/com/coffiness/calfit/storage/db/core/calendar/ScheduleAttendeeRepository.java
+++ storage/db-core/src/main/java/com/coffiness/calfit/storage/db/core/calendar/ScheduleAttendeeRepository.java
@@ -1,6 +1,8 @@
 package com.coffiness.calfit.storage.db.core.calendar;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,5 +12,8 @@ public interface ScheduleAttendeeRepository extends JpaRepository<ScheduleAttend
     List<ScheduleAttendeeEntity> findByScheduleId(Long scheduleId);
 
     // 일정이 삭제될 때 참석자 데이터도 삭제
+    // 벌크 연산 처리
+    @Modifying
+    @Query("DELETE FROM ScheduleAttendeeEntity s WHERE s.scheduleId = :scheduleId")
     void deleteByScheduleId(Long scheduleId);
 }
