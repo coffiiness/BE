25add4bd6dff9a73a6eedb662b15546b2eb99d5d
Apeirogon99 <apeirogon99@gmail.com>
2026-03-12 13:18:12 +0900

    refactor: [BE] JsonProperty 추가

--- core/domain-application/src/main/java/com/coffiness/calfit/api/v1/request/ApplicationCreateRequest.java
+++ core/domain-application/src/main/java/com/coffiness/calfit/api/v1/request/ApplicationCreateRequest.java
@@ -1,7 +1,7 @@
 package com.coffiness.calfit.api.v1.request;
 
 import com.coffiness.calfit.core.enums.Gender;
-import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -20,4 +20,4 @@ public record ApplicationCreateRequest(
     @NotNull @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate birthDate,
     @NotBlank @Size(max = 20) String phone,
     @NotBlank @Email @Size(max = 50) String email,
-    @JsonAlias("schema") @NotNull JsonNode formFields) {}
+    @JsonProperty("formFields") @NotNull JsonNode formFields) {}
